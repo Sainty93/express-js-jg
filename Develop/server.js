b370085c8abad0d17ecd673c82101ae2900571c7
Sainty93 <jenandfloyd93@icloud.com>
2023-02-20 14:17:19 -0800

    commented it out some code that might not work where it is/testing

--- Develop/server.js
+++ Develop/server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const path = require('path');
-const notes = require('./db/db.json');
+//const notes = require('./db/db.json');
 const routesApi = require('./routes/routesApi');
 const routesHtml = require('./routes/routesHtml');
 const app = express();
 const PORT = process.env.Port || 3001;
 
-app.use(express.json)());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+//app.use(express.static('public'));
 app.use('/api', routesApi);
 app.use('/', routesHtml);
-app.use(express.static(path.join(__dirname, '/api/notes')));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => 
+res.sendFile(path.join(__dirname, '/public/index.html'))); 
 
 app.listen(PORT, () => console.log('listening on PORT: ${PORT}'));
