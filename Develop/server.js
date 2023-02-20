aca06e86bf7c482bb4b760fe08cd1bd8ac31deb1
Sainty93 <jenandfloyd93@icloud.com>
2023-02-20 14:03:31 -0800

    added routes files and app.use for server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
-
+const notes = require('./db/db.json');
+const routesApi = require('./routes/routesApi');
+const routesHtml = require('./routes/routesHtml');
 const app = express();
 const PORT = process.env.Port || 3001;
+
+app.use(express.json)());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', routesApi);
+app.use('/', routesHtml);
+app.use(express.static(path.join(__dirname, '/api/notes')));
+
+app.listen(PORT, () => console.log('listening on PORT: ${PORT}'));
