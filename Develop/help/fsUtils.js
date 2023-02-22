1606262d7bfbc70a16a887e89217c417cd70b0ad
Sainty93 <jenandfloyd93@icloud.com>
2023-02-22 10:08:41 -0800

    finished helpers

--- Develop/help/fsUtils.js
+++ Develop/help/fsUtils.js
@@ -21,4 +21,14 @@ err ?  console.error(err) : console.info(`\nData written to ${destination}`)
  */
 const readAndAppend = (content, file) => {
     fs.readFile(file, 'utf8', (err, data) => {
-}
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file, parsedData);
+        }
+    });
+};
+
+module.exports = { readFromFile, writeToFile, readAndAppend };
